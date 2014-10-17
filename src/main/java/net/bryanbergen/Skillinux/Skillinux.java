6f938fc0baa827dbe22757d1995708358d01ff3f
Bryan Bergen <bryanbergen@gmail.com>
2014-10-16 20:47:38 -0700

    First round of XML parsing started. Simple calls for server status, online players, wallet balance done. Simple caching framework started.

--- src/main/java/net/bryanbergen/Skillinux/Skillinux.java
+++ src/main/java/net/bryanbergen/Skillinux/Skillinux.java
@@ -5,6 +5,9 @@ import net.bryanbergen.Skillinux.XMLParser.Parser;
 public class Skillinux {
 
     public static void main(String[] args) {
-        System.out.println(new Parser().getActivePlayerCount());
+        Parser parser = new Parser();
+        boolean online = parser.isTranquilityOnline();
+        int playerCount = parser.getActivePlayerCount();
+        System.out.println("Tranquility is " + (online ? "online with " + playerCount + " players." : "offline."));
     }
 }
--- src/main/java/net/bryanbergen/Skillinux/XMLParser/Parser.java
+++ src/main/java/net/bryanbergen/Skillinux/XMLParser/Parser.java
@@ -1,8 +1,11 @@
 package net.bryanbergen.Skillinux.XMLParser;
 
+import java.text.NumberFormat;
 import java.util.Calendar;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import net.bryanbergen.Skillinux.APICall.AccountCall;
 import net.bryanbergen.Skillinux.APICall.ApiCall;
 import net.bryanbergen.Skillinux.APICall.CharacterCall;
@@ -11,12 +14,24 @@ import net.bryanbergen.Skillinux.Entities.API;
 import net.bryanbergen.Skillinux.Entities.EveCharacter;
 import net.bryanbergen.Skillinux.Entities.Skill;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class Parser {
 
+    private static final String ONLINE_PLAYERS = "onlinePlayers";
+    private static final String SERVER_OPEN = "serverOpen";
+    private static final String BALANCE = "balance";
+    private static final String CACHE_TIME = "cacheTime";
+    private static final String CURRENT_TIME = "currentTime";
+    
     private final ApiCall caller = ApiCall.getInstance();
     private API api;
     private EveCharacter character;
+    private Map<String, String> serverQueryResults;
+    private boolean serverStatusCached;
     
     public Parser() {
     }
@@ -62,11 +77,26 @@ public class Parser {
         //TODO parse xmlDoc for account active status
         return false;
     }
-    
-    public long getWalletBalance() {
+
+    /**
+     * Returns the raw balance of an <code>EveCharacter</code>'s wallet balance.
+     * Throws <code>IllegalStateException</code> if <code>Parser</code> not 
+     * created with an <code>EveCharacter</code>. 
+     * 
+     * @return Wallet balance.
+     */
+    public double getWalletBalance() {
+        if (character == null) {
+            throw new IllegalStateException("Parser must be created with an Eve Character for CharacterCalls.");
+        }
         Document xmlDoc = caller.getCharacterDocument(character, CharacterCall.AccountBalance);
-        //TODO parse xmlDoc for wallet balance
-        return 0L;
+        String value = getAttributeValueByName(xmlDoc.getChildNodes(), BALANCE);
+        return Double.parseDouble(value);
+    }
+    
+    public String getFormattedWalletBalance() {
+        NumberFormat formatter = NumberFormat.getCurrencyInstance();
+        return formatter.format(getWalletBalance());
     }
     
     public Skill getSkillInTraining() {
@@ -81,15 +111,133 @@ public class Parser {
         return null;
     }
     
+    /**
+     * Query the API server for the status of Tranquility
+     * 
+     * @return True if Tranquility is online, false otherwise. 
+     */
     public boolean isTranquilityOnline() {
-        Document xmlDoc = caller.getServerDocument(ServerCall.ServerStatus);
-        //TODO parse xmlDoc for server status
-        return false;
+        if (updateServerQueryCache()) {
+            cacheServerStatus();
+        }
+        return Boolean.valueOf(serverQueryResults.get(SERVER_OPEN));
     }
     
+    /**
+     * Query the API server to get the active player count.
+     * 
+     * @return Number of players online on Tranquility.
+     */
     public int getActivePlayerCount() {
+        if (updateServerQueryCache()) {
+            cacheServerStatus();
+        }
+        return Integer.parseInt(serverQueryResults.get(ONLINE_PLAYERS));
+    }
+    
+    private void cacheServerStatus() {
         Document xmlDoc = caller.getServerDocument(ServerCall.ServerStatus);
-        //TODO parse xmlDoc for active player count;
-        return 0;
+        NodeList nodes = xmlDoc.getDocumentElement().getChildNodes();
+        Node playerCount = getNodeByName(nodes, ONLINE_PLAYERS);
+        Node serverOpen = getNodeByName(nodes, SERVER_OPEN);
+        
+        if (serverQueryResults == null) {
+            serverQueryResults = new HashMap<String, String>();
+        }
+        
+        serverQueryResults.put(CURRENT_TIME, Calendar.getInstance().getTimeInMillis() + "");
+        serverQueryResults.put(ONLINE_PLAYERS, playerCount.getLastChild().getTextContent());
+        serverQueryResults.put(SERVER_OPEN, serverOpen.getLastChild().getTextContent());
+        serverStatusCached = true;
+    }
+    
+    private boolean updateServerQueryCache() {
+        //TODO Flesh this out so it tracks the time the query was cached, and checks if it should be refreshed.
+        return !serverStatusCached;
+    }
+    
+    /**
+     * Convenience method for getting shallow nodes from NodeLists.
+     * <b>This method will not search deeper than one Node</b>.
+     * 
+     * @param nodes Found by <code>getDocumentElement().getChildNodes()</code>
+     * @param name Search parameter
+     * @return Corresponding <code>Node</code> if found, null otherwise.
+     */
+    private Node getNodeByName(NodeList nodes, String name) {
+        Node n = null;
+        outerloop:
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node node = nodes.item(i);
+            if (node instanceof Element && node.hasChildNodes()) {
+                NodeList children = node.getChildNodes();
+                for (int k = 0; k < children.getLength(); k++) {
+                    Node child = children.item(k);
+                    if (child instanceof Element) {
+                        if (child.getNodeName().equals(name)) {
+                            n = child;
+                            break outerloop;
+                        }
+                    }
+                }
+            }
+        }
+        return n;
+    }
+    
+    private String getAttributeValueByName(NodeList nodeList, String name) {
+        String value = "";
+        outerloop:
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            if (!value.equals("")) {
+                break;
+            }
+            Node tempNode = nodeList.item(i);
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+                if (tempNode.hasAttributes()) {
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+                    for (int k = 0; k < nodeMap.getLength(); k++) {
+                        Node node = nodeMap.item(k);
+                        if (node.getNodeName().equals(name)) {
+                            value = node.getNodeValue();
+                            break outerloop;
+                        }
+                    }
+                }
+                if (tempNode.hasChildNodes()) { 
+                    value = getAttributeValueByName(tempNode.getChildNodes(), name);
+                }
+            }
+        }
+        return value;
+    }
+    
+    
+    /**
+     * Debugging method for examining the nodes of an XMLDocument. 
+     * This method will recursively traverse the <code>NodeList</code>
+     * 
+     * @param nodeList 
+     */
+    private void printOutXML(NodeList nodeList) {
+        for (int count = 0; count < nodeList.getLength(); count++) {
+            Node tempNode = nodeList.item(count);
+            if (tempNode.getNodeType() == Node.ELEMENT_NODE) {
+                System.out.println("\nNode Name = " + tempNode.getNodeName() + " [OPEN]");
+                System.out.println("Node Value = " + tempNode.getTextContent());
+                if (tempNode.hasAttributes()) {
+                    NamedNodeMap nodeMap = tempNode.getAttributes();
+                    for (int i = 0; i < nodeMap.getLength(); i++) {
+                        Node node = nodeMap.item(i);
+                        System.out.println("Attribute name : " + node.getNodeName());
+                        System.out.println("Attribute value : " + node.getNodeValue());
+                    }
+                }
+                if (tempNode.hasChildNodes()) {
+                    printOutXML(tempNode.getChildNodes());
+                }
+                System.out.println("Node Name = " + tempNode.getNodeName() + " [CLOSE]");
+            }
+        }
     }
 }
